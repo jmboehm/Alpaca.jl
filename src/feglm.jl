4d6fc94359191f1be305c8b59c9b2206dc40e28e
Johannes Boehm <johannes.boehm@gmail.com>
2019-03-02 15:50:38 +0100

    Hacky temporary solution with string interface

--- src/feglm.jl
+++ src/feglm.jl
@@ -1,51 +1,83 @@
 
+# Ideally, use a similar interface as GLM.jl or FixedEffectModels.jl
+# But now we're lazy and use strings instead...
 
-function feglm(df::AbstractDataFrame, m::Model; kwargs...)
-    feglm(df, m.f; m.dict..., kwargs...)
-end
+# function feglm(df::AbstractDataFrame, m::Model; kwargs...)
+#     feglm(df, m.f; m.dict..., kwargs...)
+# end
 
-function feglm(df::AbstractDataFrame, f::Formula;
-    fe::Union{Symbol, Expr, Nothing} = nothing,
-    vcov::Union{Symbol, Expr, Nothing} = :(simple()),
-    weights::Union{Symbol, Expr, Nothing} = nothing,
-    subset::Union{Symbol, Expr, Nothing} = nothing,
-    maxiter::Integer = 10000, contrasts::Dict = Dict(),
-    tol::Real= 1e-8, df_add::Integer = 0,
-    save::Union{Bool, Symbol} = false,  method::Symbol = :lsmr, drop_singletons = true
-   )
-    
-    # @rput points
-    
-    # R"library('fastcluster')"
-    # if method == :ward2
-    #     R"clusters <- hclust(dist(points), \"ward.D2\")"
-    # elseif method == :ward1
-    #     R"clusters <- hclust(dist(points), \"ward.D\")"
-    # end
-    
-    # R"clusterCut <- cutree(clusters, $nclusters)"
-    # @rget clusterCut
-    
-    # return clusterCut
+# function feglm(df::AbstractDataFrame, f::Formula;
+#     fe::Union{Symbol, Expr, Nothing} = nothing,
+#     vcov::Union{Symbol, Expr, Nothing} = :(simple()),
+#     weights::Union{Symbol, Expr, Nothing} = nothing,
+#     subset::Union{Symbol, Expr, Nothing} = nothing,
+#     maxiter::Integer = 10000, contrasts::Dict = Dict(),
+#     tol::Real= 1e-8, df_add::Integer = 0,
+#     save::Union{Bool, Symbol} = false,  method::Symbol = :lsmr, drop_singletons = true
+#    )
 
+#     # Check fixed effect formula and construct string
+#     feformula = fe
+#     fe_str = "" 
+#     has_absorb = feformula != nothing
+#     if has_absorb
+#         # check depth 1 symbols in original formula are all CategoricalVector
+#         if isa(feformula, Symbol)
+#             x = feformula
+#             !isa(df[x], CategoricalVector) && error("$x should be CategoricalVector")
+#             fe_str = String(fe)
+#         elseif feformula.args[1] == :+
+#             x = feformula.args
+#             for i in 2:length(x)
+#                 isa(x[i], Symbol) && !isa(df[x[i]], CategoricalVector) && error("$(x[i]) should be CategoricalVector")
+#                 fe_str = "$fe_str + $(String(x[i]))"
+#             end
+#         end
+#     end
     
-    R"""
-    library(alpaca)
-    library(readstata13)
-    """
+#     R"library(alpaca)"
     
+#     println("FE String: $fe_str")
+
+#     r_df = robject(df)
+#     r_f = robject(f)
+#     @rput f
+#     @rput fe_str
+    
+#     # Julia's formulae don't have the FE added. 
+#     # For some reason, this does not work (brackets screw it up)
+#     #R"f <- update(f, paste(\"~ . |\",fe_str ) )"
+
+#     # for some reason, this works though:
+#     #R"f <- as.formula(binary ~ SepalWidth | SpeciesDummy)"
+
+#     # confirm what we're running
+#     R"f_string <- as.character(f)"
+#     f_str = @rget f_string
+#     println("Running alpaca with formula: $f_str")
+
+#     # ctrl <- feglm.control(step.tol = 1e-07, dev.tol = 1e-07,
+#     #                        pseudo.tol = 1e-07, rho.tol = 1e-04, iter.max = 100L, trace = 2L,
+#     #                        drop.pc = TRUE)
+#     R"mod <- feglm(formula =  f , data = $r_df, family = binomial())"
+
+#     @rget mod
+# end
+
+# Interface with strings. Hacky, and a temporary solution.
+function feglm(df::AbstractDataFrame, ms::String)
+
+    # load alpaca in R
+    R"library(alpaca)"
+
     r_df = robject(df)
-    r_f = robject(f)
+    @rput ms
+    R"f <- as.formula(ms)"
 
-    R"summary($r_df)"
-    
-    # ctrl <- feglm.control(step.tol = 1e-07, dev.tol = 1e-07,
-    #                        pseudo.tol = 1e-07, rho.tol = 1e-04, iter.max = 100L, trace = 2L,
-    #                        drop.pc = TRUE)
-    R"mod <- feglm(formula = $r_f , data = $r_df, family = binomial())"
+    # run it
+    R"mod <- feglm(formula =  f , data = $r_df, family = binomial())"
 
+    # return output
     a = @rget mod
-    
-
 
 end
--- test/alpaca.jl
+++ test/alpaca.jl
@@ -3,7 +3,11 @@ include("src/alpaca.jl")
 using GLM, RDatasets, Test
 
 df = dataset("datasets", "iris")
+df.binary = vec(Float64.(rand(0:1,size(df,1),1)))
+df[:SpeciesDummy] = categorical(df[:Species])
 
-m = Alpaca.@model(SepalLength ~ SepalWidth)
+m = Alpaca.@model(SepalLength ~ SepalWidth, fe = SpeciesDummy)
 
-rr1 = Alpaca.feglm(df, Alpaca.@model(SepalLength ~ SepalWidth))
+rr1 = Alpaca.feglm(df, Alpaca.@model(binary ~ SepalWidth, fe = SpeciesDummy))
+
+rr1 = Alpaca.feglm(df, "binary ~ SepalWidth | SpeciesDummy")
