e22720e348e1b7225155460bb767855373e9f5e0
Johannes Boehm <johannes.boehm@gmail.com>
2019-11-14 00:59:24 +0100

    First updated version

--- src/alpaca.jl
+++ src/alpaca.jl
@@ -13,13 +13,9 @@ module Alpaca
     using DataFrames
     using Distributions
     using Printf
-    # using StatsBase: CoefTable, StatisticalModel, RegressionModel
-    # import Printf: @sprintf
-    # import DataFrames: DataFrame, AbstractDataFrame, completecases, names!, ismissing
     import StatsBase: coef, nobs, coeftable, vcov, predict, residuals, var,
         RegressionModel, model_response, stderror, confint, fit, CoefTable,
         dof_residual, r2, adjr2, deviance, mss, rss, islinear, response
-    # import StatsModels: @formula,  FormulaTerm, ModelFrame, ModelMatrix, coefnames
     using Reexport
     @reexport using StatsBase
     @reexport using StatsModels
@@ -38,9 +34,11 @@ module Alpaca
     ##
     ##############################################################################
 
-    include("model.jl")
     include("RegressionResult.jl")
     include("feglm.jl")
 
+    # instantiate R's alpaca package
+    R"if(\"alpaca\" %in% rownames(installed.packages()) == FALSE) {install.packages(\"alpaca\")}"
+    R"if(\"formula.tools\" %in% rownames(installed.packages()) == FALSE) {install.packages(\"formula.tools\")}"
 
 end
--- test/alpaca.jl
+++ test/alpaca.jl
@@ -1,6 +1,4 @@
-include("src/alpaca.jl")
-
-using RDatasets, Test, Distributions
+using Alpaca, RDatasets, Test, Distributions
 
 df = dataset("datasets", "iris")
 df.binary = vec(Float64.(rand(0:1,size(df,1),1)))
@@ -10,38 +8,42 @@ a = ["A","B","C"]
 df.Random = vec([a[i] for i in idx])
 df.RandomCategorical = categorical(df[!,:Random])
 
-#
-# m = Alpaca.@model(SepalLength ~ SepalWidth, fe = SpeciesDummy)
-#
-# rr1 = Alpaca.feglm(df, Alpaca.@model(binary ~ SepalWidth, fe = SpeciesDummy))
+# proper interface
 
 # binomial/logit
 result = Alpaca.feglm(df, Alpaca.@formula(binary ~ SepalWidth),
     Binomial(),
     fe = :SpeciesDummy
     )
+@test StatsBase.coef(result) ≈ [-1.00555] atol = 1e-4
+# with clustering
 result = Alpaca.feglm(df, Alpaca.@formula(binary ~ SepalWidth),
     Binomial(),
     fe = :SpeciesDummy, vcov = :(cluster(SpeciesDummy))
     )
+@test StatsBase.stderror(result) ≈ [0.464767] atol = 1e-4
+# two-way clustering
 result = Alpaca.feglm(df, Alpaca.@formula(binary ~ SepalWidth),
     Binomial(),
     fe = :SpeciesDummy, vcov = :(cluster(SpeciesDummy + RandomCategorical))
-    );
-
+    )
+@test StatsBase.stderror(result) ≈ [0.446949] atol = 1e-4
 # poisson
-rr1 = Alpaca.feglm(df, Alpaca.@formula(binary ~ SepalWidth),
+result = Alpaca.feglm(df, Alpaca.@formula(binary ~ SepalWidth),
     Poisson(),
     fe = :SpeciesDummy
     )
+@test StatsBase.coef(result) ≈ [-0.507638] atol = 1e-4
+# two fe's
+result = Alpaca.feglm(df, Alpaca.@formula(binary ~ SepalWidth),
+    Poisson(),
+    fe =:(SpeciesDummy + RandomCategorical)
+    )
+@test StatsBase.coef(result) ≈ [-0.502443] atol = 1e-4
 
 
 # string interface
 rr1 = Alpaca.feglm(df, "binary ~ SepalWidth | SpeciesDummy", "Binomial")
-
-
-#
-# using FixedEffectModels
-# reg(df, @model(SepalLength ~ SepalWidth, fe = SpeciesDummy, vcov = cluster(SpeciesDummy)))
-#
-# m = @model(SepalLength ~ SepalWidth, fe = SpeciesDummy, vcov = cluster(SpeciesDummy))
+@test StatsBase.coef(rr1) ≈ [-1.00555] atol = 1e-4
+rr1 = Alpaca.feglm(df, "binary ~ SepalWidth | SpeciesDummy", "Binomial", vcov = :(cluster(SpeciesDummy)))
+@test StatsBase.stderror(rr1) ≈ [0.464767] atol = 1e-4
